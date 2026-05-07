37a42d77b6da662a70f1f1e76c88d4f69e33f81a
Will Dee <william.r.d@protonmail.com>
2026-05-07 15:04:16 +1000

    Add telescope find and grep to neo-tree keybindings
    'tf' and 'tg' now open Telescope for finding files and live grep scoped
    to the in-focus directory in neo-tree.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -18,7 +18,27 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          -- Map 'tf' to Telescope find_files in the current directory
+          ['tf'] = 'telescope_find',
+          -- Map 'tg' to Telescope live_grep in the current directory
+          ['tg'] = 'telescope_grep',
         },
+      },
+      commands = {
+        telescope_find = function(state)
+          local node = state.tree:get_node()
+          local path = node.type == 'directory' and node:get_id() or vim.fn.fnamemodify(node:get_id(), ':h')
+          require('telescope.builtin').find_files {
+            cwd = path,
+          }
+        end,
+        telescope_grep = function(state)
+          local node = state.tree:get_node()
+          local path = node.type == 'directory' and node:get_id() or vim.fn.fnamemodify(node:get_id(), ':h')
+          require('telescope.builtin').live_grep {
+            cwd = path,
+          }
+        end,
       },
     },
   },
